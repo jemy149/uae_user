7da2be8c8ae43e23544c1e10b32aa3081ca849ce
Said Gamal <said12045@gmail.com>
2022-05-25 23:27:39 +0200

    28.5

--- lib/presentation/screens/user_screens/delivery_information/delivery_information_screen.dart
+++ lib/presentation/screens/user_screens/delivery_information/delivery_information_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uae_user/data/models/user_models/get_orders/get_orders_model.dart';
 import 'package:uae_user/presentation/views/delivery_information_card_item.dart';
 
 import '../../../../constants/constants.dart';
@@ -12,7 +13,8 @@ import '../../../widgets/default_map.dart';
 import '../../../widgets/default_text.dart';
 
 class DeliveryInformationScreen extends StatefulWidget {
-  const DeliveryInformationScreen({Key? key}) : super(key: key);
+  final GetOrdersModel getOrdersModel;
+  DeliveryInformationScreen({Key? key, required this.getOrdersModel}) : super(key: key);
 
   @override
   State<DeliveryInformationScreen> createState() => _DeliveryInformationScreenState();
@@ -82,10 +84,101 @@ class _DeliveryInformationScreenState extends State<DeliveryInformationScreen> {
               ),
               Column(
               mainAxisAlignment: MainAxisAlignment.end,
-              children:const [
+              children: [
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 15.0,horizontal: 20),
-                  child: DeliveryInformationCardItem(),
+                  child: Card(
+                    color: AppColors.lightBlue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 15),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              DefaultText(
+                                text: AppLocalizations.of(context)!.orderCost,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    ?.copyWith(color: Colors.white),
+                              ),
+                              DefaultText(
+                                text: '${000} AED',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    ?.copyWith(color: AppColors.lightGrey),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsetsDirectional.only(top: 10.0),
+                            child: Row(
+                              children: [
+                                Material(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(1000)),
+                                  clipBehavior: Clip.antiAlias,
+                                  child: SizedBox(
+                                      height: 80,
+                                      width: 80,
+                                      child: Image.asset(
+                                        'assets/images/chocolate.png',
+                                        fit: BoxFit.cover,
+                                      )),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsetsDirectional.only(start: 5.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        DefaultText(
+                                          text: 'No Driver',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline6
+                                              ?.copyWith(color: Colors.white),
+                                        ),
+                                        DefaultText(
+                                          text: 'Delivery Representative',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1
+                                              ?.copyWith(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () {},
+                                  child:  Card(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15)
+                                    ),
+                                    color: AppColors.lightBlue,
+                                    elevation: 20,
+                                    child:const  Padding(
+                                      padding: EdgeInsets.all(15.0),
+                                      child: Icon(
+                                        Icons.phone,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ],
             )],
